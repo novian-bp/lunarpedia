b93894b418c899e04d187a6feb4818b334798167
Andika <94437486+novian-bp@users.noreply.github.com>
2025-06-07 19:23:35 +0700

    Fix Vite configuration for React project

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: true
+  }
+})
